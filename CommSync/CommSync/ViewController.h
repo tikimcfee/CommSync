769c02d172a8619d2902b1e7e40b64e08e6c75d3
Ivan Lugo <ivan.lugo@sightplan.com>
2014-09-30 16:06:04 -0400

    Initial Project Creation
    So it begins.

---
+++ CommSync/CommSync/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommSync
+//
+//  Created by Ivan Lugo on 9/30/14.
+//  Copyright (c) 2014 AppsByDLI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CommSync/CommSync/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CommSync
+//
+//  Created by Ivan Lugo on 9/30/14.
+//  Copyright (c) 2014 AppsByDLI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
