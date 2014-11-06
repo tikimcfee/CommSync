8a206e8ea0b523416204ddf0dc892ac37418dbc7
UCF_iMac <commsync-group@Students-iMac.local>
2014-11-05 19:10:14 -0500

    Task and user views created

---
+++ CommSync/CommSync/ViewControllers/CSUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSUserViewController.h
+//  CommSync
+//
+//  Created by CommSync on 11/5/14.
+//  Copyright (c) 2014 AppsByDLI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSUserViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
