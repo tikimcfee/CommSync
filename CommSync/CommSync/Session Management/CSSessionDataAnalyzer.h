46378209bee06eba2a57098e77b3ac59b7a34fc7
CommSync_ivan <iheartlappy486@gmail.com>
2015-02-18 12:18:52 -0500

    Data analyzer class for handling reception, analysis, and corresponding actions to MCSession objects. Cleaned up project build targets to stop unnecessary warnings from JKConnectivity.

---
+++ CommSync/CommSync/Session Management/CSSessionDataAnalyzer.h
@@ -0,0 +1,13 @@
+//
+//  CSSessionDataAnalyzer.h
+//  CommSync
+//
+//  Created by CommSync on 2/18/15.
+//  Copyright (c) 2015 AppsByDLI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSSessionDataAnalyzer : NSObject
+
+@end
---
+++ CommSync/CommSync/Session Management/CSSessionDataAnalyzer.m
@@ -0,0 +1,13 @@
+//
+//  CSSessionDataAnalyzer.m
+//  CommSync
+//
+//  Created by CommSync on 2/18/15.
+//  Copyright (c) 2015 AppsByDLI. All rights reserved.
+//
+
+#import "CSSessionDataAnalyzer.h"
+
+@implementation CSSessionDataAnalyzer
+
+@end
