1daa139e85e07b198e2acddaa3e3ab86a670ebc2
michael <student@Students-iMac-2.local>
2015-02-23 18:57:43 -0500

    message View controller, detail color

---
+++ CommSync/CSMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSMessageViewController.h
+//  CommSync
+//
+//  Created by Student on 2/23/15.
+//  Copyright (c) 2015 AppsByDLI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSMessageViewController : UITableViewController
+
+@end
